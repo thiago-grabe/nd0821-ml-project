19234520d526bef7c39da3db057c9278d13b6b17
Thiago Grabe <tmgmeireles@hotmail.com>
2024-10-20 20:27:40 -0300

    try: removing components.

--- components/get_data/run.py
+++ components/get_data/run.py
@@ -7,7 +7,7 @@ import logging
 import os
 import wandb
 
-from components.wandb_utils.log_artifact import log_artifact
+from wandb_utils.log_artifact import log_artifact
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
