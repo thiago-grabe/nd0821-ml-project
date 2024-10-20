3ab5d9c99c731e08180b063d597d9eda97cfaa3f
Thiago Grabe <tmgmeireles@hotmail.com>
2024-10-20 20:38:10 -0300

    try: removing components.

--- components/wandb_utils/setup.py
+++ components/wandb_utils/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name="wandb-utils",
+    name="wandb_utils",
     version=0.1,
     description="Utilities for interacting with Weights and Biases and mlflow",
     zip_safe=False,  # avoid eggs, which make the handling of package data cumbersome
