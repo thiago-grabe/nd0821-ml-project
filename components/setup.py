6ca072a396eee40d6f6c33d6e73b6166f08cc4ad
Thiago Grabe <tmgmeireles@hotmail.com>
2024-10-20 20:12:29 -0300

    try: adjusting wandb-utils package name

--- components/setup.py
+++ components/setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 
 setup(
-    name="wandb_utils",
+    name="wandb-utils",
     version=0.1,
-    description="Utilities for interacting with Weights and Biases and mlflow",\
-    packages=find_packages(),
+    description="Utilities for interacting with Weights and Biases and mlflow",
+    zip_safe=False,  # avoid eggs, which make the handling of package data cumbersome
+    packages=["wandb_utils"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
