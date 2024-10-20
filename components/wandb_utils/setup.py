cca3bf33d10b9b34289ab63a5c83bb953e15ba46
Thiago Grabe <tmgmeireles@hotmail.com>
2024-10-20 20:04:20 -0300

    try: moving setup.py to wandb_utils folder

--- components/wandb_utils/setup.py
+++ components/wandb_utils/setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name="wandb_utils",
     version=0.1,
     description="Utilities for interacting with Weights and Biases and mlflow",
     zip_safe=False,  # avoid eggs, which make the handling of package data cumbersome
-    packages=["wandb_utils"],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
